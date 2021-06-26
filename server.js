2199094c4d77c8ce674ebec8b7fb9919352cec41
gulcer <84224250+gulcer@users.noreply.github.com>
2021-06-26 13:30:36 +0300

    new 26.02

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = 8000;
+
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  const user = {
-    name: "John"
-  };
-  
-  user.name = "Pete"; // (*)
-  
-  res.send(user)
+app.post('/send-mail', (req, res) => {
+
+  fs.writeFile('out.json', JSON.stringify(req.body), 'utf8', () => {
+    console.log('Мы записали данные, ура!')
+  });
+  res.send({
+    status: 'prishlo'
+  })
 });
 
-// listen on the port
 app.listen(port);
